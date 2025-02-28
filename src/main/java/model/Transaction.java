dbe860fef639164e7b0e8f73409e2d2ceb4bc381
AlexandrLikhanov <u2t1frr@gmail.com>
2025-02-28 17:41:33 +0900

    adds model Category.java realisation

--- src/main/java/model/Transaction.java
+++ src/main/java/model/Transaction.java
@@ -1,28 +1,40 @@
 package model;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
 
 import java.time.LocalDate;
 import java.util.Locale;
 
 
-@Getter
-@Setter
+@Entity
+@Getter @Setter
+@NoArgsConstructor @AllArgsConstructor
 @Table(name = "transactions")
 public class Transaction {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "amount", nullable = false)
     private double amount;
+
+    @Column(name = "description")
     private String description;
+
+    @Column(name = "date", nullable = false)
     private LocalDate date;
 
     @ManyToOne
     @JoinColumn(name = "category_id")
-    private Locale.Category category;
+    private Category category;
 
 
 }

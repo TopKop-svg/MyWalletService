2078d3f5cac1a1b8b50c36b70112997994bb23b3
AlexandrLikhanov <u2t1frr@gmail.com>
2025-03-01 23:19:19 +0900

    added CategoryRepository.java/ TransactionRepository.java interfaces

--- src/main/java/org/example/MyWalletServiceApplication.java
+++ src/main/java/org/example/MyWalletServiceApplication.java
@@ -1,11 +1,45 @@
 package org.example;
 
+import org.example.model.Category;
+import org.example.model.Transaction;
+import org.example.repository.CategoryRepository;
+import org.example.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.time.LocalDate;
+
 @SpringBootApplication
-public class MyWalletServiceApplication {
+public class MyWalletServiceApplication implements CommandLineRunner {
+    private final CategoryRepository categoryRepository;
+    private final TransactionRepository transactionRepository;
+
+    @Autowired
+    public MyWalletServiceApplication(CategoryRepository categoryRepository, TransactionRepository transactionRepository) {
+        this.categoryRepository = categoryRepository;
+        this.transactionRepository = transactionRepository;
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(MyWalletServiceApplication.class, args);
+    }
+
+    @Override
+    public void run(String...args) throws Exception {
+        Category food = new Category();
+        food.setName("Food");
+        categoryRepository.save(food);
+
+        Transaction transaction = new Transaction();
+        transaction.setAmount(50.0);
+        transaction.setDescription("Lunch");
+        transaction.setDate(LocalDate.now());
+        transaction.setCategory(food);
+        transactionRepository.save(transaction);
+
+        System.out.println("Categories: " + categoryRepository.findAll());
+        System.out.printf("Transactions: " + transactionRepository.findAll());
     }
 }

8e343133b68633dfb4b9fb5e64f67432051018ea
AlexandrLikhanov <u2t1frr@gmail.com>
2025-03-01 23:24:14 +0900

    work with REST

---
+++ src/main/java/org/example/controller/CategoryController.java
@@ -0,0 +1,28 @@
+package org.example.controller;
+
+import org.example.model.Category;
+import org.example.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/categories")
+public class CategoryController {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @GetMapping
+    public List<Category> getAllCategories() {
+        return categoryRepository.findAll();
+    }
+
+    @PostMapping
+    public ResponseEntity<Category> createCategory(@RequestBody Category category) {
+        Category savedCategory = categoryRepository.save(category);
+        return ResponseEntity.ok(savedCategory);
+    }
+}

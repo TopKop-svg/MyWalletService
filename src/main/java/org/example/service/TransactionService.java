0a293f2e7e8f8b4a602c7270d6df3eaef3a78fd2
AlexandrLikhanov <u2t1frr@gmail.com>
2025-03-03 21:55:19 +0900

    TransactionService is done

--- src/main/java/org/example/service/TransactionService.java
+++ src/main/java/org/example/service/TransactionService.java
@@ -1,4 +1,49 @@
 package org.example.service;
 
+import org.example.model.Transaction;
+import org.example.repository.TransactionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class TransactionService {
+
+    private final TransactionRepository transactionRepository;
+
+    @Autowired
+    public TransactionService(TransactionRepository transactionRepository) {
+        this.transactionRepository = transactionRepository;
+    }
+
+    public List<Transaction> getAllTransactions() {
+        return transactionRepository.findAll();
+    }
+
+    public Transaction createTransaction(Transaction transaction) {
+        return transactionRepository.save(transaction);
+    }
+
+    public Optional<Transaction> updateTransaction(Long id, Transaction transactionDetails) {
+        return transactionRepository.findById(id)
+                .map(transaction -> {
+                    transaction.setAmount(transactionDetails.getAmount());
+                    transaction.setDescription(transactionDetails.getDescription());
+                    transaction.setDate(transactionDetails.getDate());
+                    transaction.setCategory(transactionDetails.getCategory());
+                    return transactionRepository.save(transaction);
+                });
+    }
+
+    public boolean deleteTransaction(Long id) {
+        return transactionRepository.findById(id)
+                .map(transaction -> {
+                    transactionRepository.delete(transaction);
+                    return true;
+                })
+                .orElse(false);
+    }
+
 }

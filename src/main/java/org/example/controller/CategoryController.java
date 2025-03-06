0e94381d94c05c8a07251b77dcdaefd195981488
AlexandrLikhanov <u2t1frr@gmail.com>
2025-03-07 01:46:20 +0900

    Swagger init

--- src/main/java/org/example/controller/CategoryController.java
+++ src/main/java/org/example/controller/CategoryController.java
@@ -1,5 +1,8 @@
 package org.example.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.example.model.Category;
 import org.example.repository.CategoryRepository;
@@ -12,6 +15,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/categories")
+@Tag(name = "Category API", description = "Operations for managing categories")
 public class CategoryController {
 
     @Autowired
@@ -19,17 +23,26 @@ public class CategoryController {
     @Autowired
     private CategoryService categoryService;
 
+    @Operation(summary = "Get all categories", description = "Returns a list of all categories")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved categories")
     @GetMapping
     public List<Category> getAllCategories() {
         return categoryRepository.findAll();
     }
 
+    @Operation(summary = "Create a new category", description = "Create the new category with")
+    @ApiResponse(responseCode = "200", description = "Category create successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid category data")
     @PostMapping
     public ResponseEntity<Category> createCategory(@Valid @RequestBody Category category) {
         Category savedCategory = categoryRepository.save(category);
         return ResponseEntity.ok(savedCategory);
     }
 
+    @Operation(summary = "Update an existing category", description = "Updates the category with the given ID")
+    @ApiResponse(responseCode = "200", description = "Category updated successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid category data")
+    @ApiResponse(responseCode = "404", description = "Category not found")
     @PutMapping("/{id}")
     public ResponseEntity<Category> updateCategory(@PathVariable Long id, @Valid @RequestBody Category categoryDetails) {
         return categoryService.updateCategory(id, categoryDetails)
@@ -37,6 +50,9 @@ public class CategoryController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Update an existing category", description = "Updates the category with the given ID")
+    @ApiResponse(responseCode = "200", description = "Category deleted successfully")
+    @ApiResponse(responseCode = "404", description = "Category not found")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCategory(@PathVariable Long id) {
         return categoryService.deleteCategory(id)
--- src/main/java/org/example/controller/TransactionController.java
+++ src/main/java/org/example/controller/TransactionController.java
@@ -1,5 +1,8 @@
 package org.example.controller;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.example.model.Transaction;
 import org.example.repository.TransactionRepository;
@@ -13,23 +16,37 @@ import java.util.List;
 
 @RestController
 @RequestMapping ("/api/transactions")
+@Tag(name = "Transaction API", description = "Operations for managing transactions")
 public class TransactionController {
-    @Autowired
-    private TransactionRepository transactionRepository;
-    @Autowired
-    private TransactionService transactionService;
+    private final TransactionRepository transactionRepository;
+    private final TransactionService transactionService;
 
+    @Autowired
+    public TransactionController(TransactionRepository transactionRepository, TransactionService transactionService) {
+        this.transactionRepository = transactionRepository;
+        this.transactionService = transactionService;
+    }
+
+    @Operation(summary = "Get all transactions", description = "Returns a list of all ")
+    @ApiResponse(responseCode = "200", description = "Successfully retrieved transaction")
     @GetMapping
     public List<Transaction> getAllTransactions() {
         return transactionRepository.findAll();
     }
 
+    @Operation(summary = "Create a new transactions", description = "Creates a new transaction")
+    @ApiResponse(responseCode = "200", description = "Transaction created successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid transaction data")
     @PostMapping
     public ResponseEntity<Transaction> createTransactions(@Valid @RequestBody Transaction transaction) {
         Transaction savedTransaction = transactionRepository.save(transaction);
         return ResponseEntity.ok(savedTransaction);
     }
 
+    @Operation(summary = "Update an exising transactions", description = "Updates the transaction with the given ID")
+    @ApiResponse(responseCode = "200", description = "Transaction updated successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid transaction data")
+    @ApiResponse(responseCode = "404", description = "Transaction not found")
     @PutMapping("/{id}")
     public ResponseEntity<Transaction> updateTransaction(@PathVariable Long id,
                                                          @Valid
@@ -39,6 +56,9 @@ public class TransactionController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(summary = "Delete a transaction", description = "Delete the transaction")
+    @ApiResponse(responseCode = "200", description = "Transaction updated successfully")
+    @ApiResponse(responseCode = "404", description = "Transaction not found")
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteTransaction(@PathVariable Long id) {
         return transactionService.deleteTransaction(id)
@@ -46,6 +66,9 @@ public class TransactionController {
                 : ResponseEntity.notFound().build();
     }
 
+    @Operation(summary = "Import transactions from CSV", description = "Uploads a CSV file to import transactions")
+    @ApiResponse(responseCode = "200", description = "Transaction imported successfully")
+    @ApiResponse(responseCode = "400", description = "Invalid CSV file or data")
     @PostMapping("/import")
     public ResponseEntity<List<Transaction>> importTransactions(@RequestParam("file")MultipartFile file) {
         try {

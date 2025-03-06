fd8cacf99df7d4147fcb02c6203214ba388ff267
AlexandrLikhanov <u2t1frr@gmail.com>
2025-03-07 01:53:30 +0900

    Unit test begin

---
+++ src/main/java/org/example/service/TransactionServiceTest.java
@@ -0,0 +1,13 @@
+package org.example.service;
+
+import org.example.repository.CategoryRepository;
+import org.example.repository.TransactionRepository;
+
+public class TransactionServiceTest {
+
+    private TransactionRepository transactionRepository;
+    private CategoryRepository categoryRepository;
+    private TransactionService transactionService;
+
+
+}
